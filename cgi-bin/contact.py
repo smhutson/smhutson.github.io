abc521297440c1e0dbf3ae32e6105e012d0ae163
[01;31m[K"name"[m[K: "Stephanie Hutson <[01;31m[K"email"[m[K: null>
2016-02-08 12:05:51 -0800

    remove bad files, update contact

--- cgi-bin/contact.py
+++ cgi-bin/contact.py
@@ -1,3 +1,4 @@
-import resume
+######## Contact Information ########
 
-
+Name:  Stephanie Hutson
+Email: <a href='mailto:stephanie.m.hutson@gmail.com'>stephanie.m.hutson@gmail.com</a>
--- static/application.js
+++ static/application.js
@@ -18,6 +18,7 @@ function downOneLine(){}
 var listAll = function(){
   $("#reader-table").prepend("<tr><td>interview.py</td><td>: A walk through a few potential interview questions </td></tr>")
   $("#reader-table").prepend("<tr><td>resume.py</td><td>: A copy of Stephanie Hutson's resume </td></tr>")
+  $("#reader-table").prepend("<tr><td>contact.txt</td><td>: How to get in contact </td></tr>")
 }
 
 var runProgram = function(name){
