4b07c5d4a10cdff0c680f11502bbb321926b964e
Stephanie Hutson <stephanie.m.hutson+work@gmail.com>
2025-06-12 18:04:23 -0700

    Add meta data

--- src/data/title-description.ts
+++ src/data/title-description.ts
@@ -7,4 +7,6 @@ export const customMetadata: CustomMetadata = {
   // If you want a custom title or description you can add it here.
   // title: "My Custom Title",
   // description: "My Custom Description",
+  title: "Stephanie Hutson UCSD Economics",
+  description: "Stephanie HutsonUCSD Economics IO UC San Diego UVA Climate Change Environmental Graduate Student",
 };
